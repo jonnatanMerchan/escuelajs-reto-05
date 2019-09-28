c1087171da35775f3110d4b8c5d542365a93e265
Jonnatan Merchan <Jonnatan.merchan@intcomext.com>
2019-09-28 11:58:40 -0500

    Solucion reto completado

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+localStorage.setItem('next_fetch', API);
 
 const getData = api => {
-  fetch(api)
+  return fetch(api)
     .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
+}   
+//Consulta Api
+const loadData = async () => {
+  const urlApi = localStorage.getItem('next_fetch')
+  try {
+    const response = await getData(urlApi)
+    const characters = response.results;
+    localStorage.setItem('next_fetch', response.info.next);
+    if(response.info.next != ""){
       let output = characters.map(character => {
         return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
+            <article class="Card">
+              <img src="${character.image}" />
+              <h2>${character.name}<span>${character.species}</span></h2>
+            </article>
+          `
       }).join('');
       let newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+    }else {
+      let textMessage = document.createElement('h1');
+      textMessage.classList.add('Items');
+      textMessage.innerHTML ='Ya no hay personajes...';
+      $app.appendChild(textMessage);
+      $observe.parentNode.removeChild('observe');
+    }
 
-const loadData = () => {
-  getData(API);
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
@@ -36,3 +49,7 @@ const intersectionObserver = new IntersectionObserver(entries => {
 });
 
 intersectionObserver.observe($observe);
+
+window.unload = function(e) {
+  localStorage.clear
+};
